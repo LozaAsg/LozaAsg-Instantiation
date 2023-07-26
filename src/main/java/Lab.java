0230d97bb0011921dafa53f4e23256d2bd217c9f
Loza Asgedom <loza.mesfin777@gmail.com>
2023-07-26 05:19:19 +0000

    Coding Lab: Instantiantion

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab instantiateANewObject = new Lab();
+        return instantiateANewObject;
     }
 }
